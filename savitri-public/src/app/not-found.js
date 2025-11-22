54814da3292a003ac4389417eea92e15e57f97ba
Sachinlearnsdev <singhsachin09820@gmail.com>
2025-11-22 12:29:05 +0000

    Initiation

---
+++ savitri-public/src/app/not-found.js
@@ -0,0 +1,9 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div style={{padding: 24, textAlign: "center"}}>
+      <h1>Page not found</h1>
+      <p>The page you’re looking for doesn’t exist.</p>
+    </div>
+  );
+}
